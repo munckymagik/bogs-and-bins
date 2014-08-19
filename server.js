af0e5fe3eb5dc8cec458eade3c2609c6bb9415dc
Dan Munckton <daniel.munckton@neo.com>
2014-08-19 16:05:42 +0100

    Add note on source of info for the static file server

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+/*
+ * Make a simple static file server with Node
+ *  See: http://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
+ */
+
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080);
